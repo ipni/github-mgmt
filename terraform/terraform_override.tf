c7f6cfe6369c74e6fc3f8001af6f535f43f80ba5
Masih H. Derkani <m@derkani.org>
2022-11-17 21:57:51 +0000

    Set tf workspace key prefix
    CI jobs are hardcoded to TF_WORKSPACE.

--- terraform/terraform_override.tf
+++ terraform/terraform_override.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     region         = "us-east-2"
     bucket         = "sti-dev-terraform-state"
-    key            = "ipni/github-mgmt/terraform.tfstate"
+    key            = "terraform.tfstate"
+    workspace_key_prefix = "github-mgmt"
     dynamodb_table = "ipni_gh_mgmt_lock"
   }
 }
