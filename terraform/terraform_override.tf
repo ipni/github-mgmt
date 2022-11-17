dc98fc440014a6aaa44458e78118354c41b283dc
Masih H. Derkani <m@derkani.org>
2022-11-17 21:35:13 +0000

    Configure for `ipni`
    Replace placeholders for `ipni` organization.

--- terraform/terraform_override.tf
+++ terraform/terraform_override.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    region               = "us-east-1"
-    bucket               = "github-mgmt"
-    key                  = "terraform.tfstate"
-    workspace_key_prefix = "org"
-    dynamodb_table       = "github-mgmt"
+    region         = "us-east-2"
+    bucket         = "sti-dev-terraform-state"
+    key            = "ipni/github-mgmt/terraform.tfstate"
+    dynamodb_table = "ipni_gh_mgmt_lock"
   }
 }
