b0741133e3ccd0a8096b6fb5a571caceb36698ce
galargh <piotr.galar@gmail.com>
2022-11-24 18:13:01 +0100

    feat: expand set of resources managed by this repo

--- terraform/locals_override.tf
+++ terraform/locals_override.tf
@@ -1,6 +1,10 @@
 locals {
   resource_types = [
     "github_repository",
-    "github_branch_protection"
+    "github_branch_protection",
+    "github_membership",
+    "github_team_repository",
+    "github_team_membership",
+    "github_team"
   ]
 }
