741d10fc4b34e391a3046b5513a179fa4de9feb8
Piotr Galar <piotr.galar@gmail.com>
2023-05-18 11:25:45 +0200

    start tracking labels through github-mgmt

--- terraform/locals_override.tf
+++ terraform/locals_override.tf
@@ -2,6 +2,7 @@ locals {
   resource_types = [
     "github_repository",
     "github_branch_protection",
+    "github_issue_label",
     "github_membership",
     "github_team_repository",
     "github_team_membership",
